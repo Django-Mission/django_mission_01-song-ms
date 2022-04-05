2375b27fda677d928fedf9bba042cfc5e1ab2ada
Minsoo Song <sms0432@gmail.com>
2022-04-05 23:46:07 +0900

    fixed views and index.html

--- first-django/config/views.py
+++ first-django/config/views.py
@@ -13,5 +13,6 @@ def lotto(request):
     lotto_num = []
     for _ in range(7):
         lotto_num.append(random.randint(1,45))
-    lotto_num_1 = random.sample(lotto_num,7)
-    return render(request, 'index.html', lotto_num_1)
+    # lotto_num_1 = random.sample(lotto_num,7)
+    context_num = {'lotto_num' : lotto_num}
+    return render(request, 'index.html', context_num)
