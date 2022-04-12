e826f12cecf71b9196d66ac025d543282af287da
Minsoo Song <sms0432@gmail.com>
2022-04-13 00:20:48 +0900

    fix views

--- first-django/config/views.py
+++ first-django/config/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 import random
 
 def lotto(request):
-    
     lotto_num = []
     for _ in range(7):
         lotto_num.append(random.randint(1,45))
-    # lotto_num_1 = random.sample(lotto_num,7)
     context_num = {'lotto_num' : lotto_num}
     return render(request, 'index.html', context_num)
