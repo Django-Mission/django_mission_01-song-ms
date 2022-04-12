f0c7ad44244a05521aad827619687e8867158bf1
Minsoo Song <sms0432@gmail.com>
2022-04-13 00:35:04 +0900

    fix views

--- first-django/config/views.py
+++ first-django/config/views.py
@@ -3,7 +3,7 @@ import random
 
 def lotto(request):
     lotto_num = []
-    for _ in range(7):
+    for _ in range(6):
         lotto_num.append(random.randint(1,45))
     context_num = {'lotto_num' : lotto_num}
     return render(request, 'index.html', context_num)
